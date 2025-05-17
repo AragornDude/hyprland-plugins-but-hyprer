d173884e30fa7bda8fe8b58fcc51d678c2b22ddf
AragornDude <aragornboi+github@gmail.com>
2025-05-16 17:23:36 -0800

    Just the barDeco.hpp for now

--- hyprbars/barDeco.hpp
+++ hyprbars/barDeco.hpp
@@ -62,6 +62,7 @@ class CHyprBar : public IHyprWindowDecoration {
     bool                      m_bButtonHovered     = false;
     std::optional<CHyprColor> m_bForcedBarColor;
     std::optional<CHyprColor> m_bForcedTitleColor;
+    std::optional<int> m_bForcedBarHeight;
 
     PHLANIMVAR<CHyprColor>    m_cRealBarColor;
 
